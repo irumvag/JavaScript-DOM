8f15c195d335508ec6005209f82b002ba7cdb803
irumvag <irumvagadanaclet@gmail.com>
2025-07-17 21:38:39 +0200

    Arranged code

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-// console.log("hello");
-// function changeAllArticleColors(){
-//     const allatticle=document.querySelectorAll("#products>article");
-//     for(let article of allatticle){
-//         article.addEventListener('click',()=>{
-//         article.classList.add('sale');
-//         console.log(article.className);
-//         document.querySelector('.sale').style.backgroundColor='red';
-//     });
-//     }
-// }
-//changeAllArticleColors();
+function changeAllArticleColors(){
+    const allArticle=document.querySelectorAll('article');
+    for(let elem of allArticle)
+    {
+        console.log(elem)
+        elem.addEventListener('click',(e)=>{
+            e.target.style.backgroundColor='#ffddd2';
+        });
+        setTimeout(()=>{elem.addEventListener('click',(e)=>{
+            e.target.style.backgroundColor='';
+        });},1000);
+    }
+}
+changeAllArticleColors();
 
 function attachBuyEvents(){
    let btn=document.body.querySelectorAll('.buy'); 
-   console.log(btn);
    for(let button of btn){
     button.addEventListener('click', function(e) {
         console.log(this.parentElement.classList.toggle('sale'));
